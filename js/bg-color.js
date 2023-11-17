ae806a86e004856db26bcc909cd9930e61b22437
Gabriela <gabyplasencia1995@gmail.com>
2023-11-17 10:36:39 +0000

    background color animation finish

--- js/bg-color.js
+++ js/bg-color.js
@@ -1,12 +1,10 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".bg-color", {
-    "--dark-blue": "#00000E",
-    "--deep-blue": "#0E0C45",
+const tlBgColor = gsap.timeline({
     scrollTrigger: {
         trigger: "#project",
-        start: "top bottom",
-        end: "50% center",
+        start: "0% bottom",
+        end: "bottom center",
         // markers: {
         //     startColor: "white",
         //     endColor: "yellow",
@@ -15,17 +13,42 @@ gsap.to(".bg-color", {
     }
 });
 
-gsap.to(".bg-color", {
-    "--dark-blue": "blue",
-    "--deep-blue": "orange",
+const tlBgColor2 = gsap.timeline({
     scrollTrigger: {
         trigger: "#contact",
         start: "top bottom",
-        end: "50% center",
-        markers: {
-            startColor: "white",
-            endColor: "yellow",
-        },
+        end: "50% 85%",
+        // markers: {
+        //     startColor: "white",
+        //     endColor: "black",
+        // },
         scrub: 1,
     }
 });
+
+tlBgColor.to(".bg-color", {
+    "--_color-top": "#00000E",
+    "--_color-medium": "#0E0C45",
+    "--_color-bottom": "#0E0C45",
+})
+    .to(".bg-color", {
+        "--_color-top": "#0E0C45",
+        "--_color-medium": "#0E0C45",
+        "--_color-bottom": "#323097",
+    })
+    .to(".bg-color", {
+        "--_color-top": "#0E0C45",
+        "--_color-medium": "#3632EC",
+        "--_color-bottom": "#8886F5",
+    })
+
+tlBgColor2.to(".bg-color", {
+    "--_color-top": "#3632EC",
+    "--_color-medium": "#8886F5",
+    "--_color-bottom": "#8886F5",
+})    
+    .to(".bg-color", {
+    "--_color-top": "#8886F5",
+    "--_color-medium": "#E66239",
+    "--_color-bottom": "#FCFF61",
+    })
--- js/bg-scene3.js
+++ js/bg-scene3.js
@@ -5,16 +5,11 @@ const tlBgScene3 = gsap.timeline({
         trigger: "#contact",
         start: "0% bottom",
         end: "50% center",
-        markers: {
-            startColor: "white",
-            endColor: "yellow",
-        },
+        // markers: {
+        //     startColor: "white",
+        //     endColor: "yellow",
+        // },
         scrub: 1,
     }
 });
 
-tlBgScene3.to(".bg-color", {
-    backgroundPosition: "bottom",
- }) //.to(".bg-color", {
-//     backgroundPosition: "bottom",
-// })
