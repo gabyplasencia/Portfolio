02363b3a7959eb539892ba7858ac79f66ae9e9fb
Gabriela <gabyplasencia1995@gmail.com>
2023-11-17 10:53:46 +0000

    test

--- js/bg-scene1.js
+++ js/bg-scene1.js
@@ -1,15 +1,15 @@
-const lenis = new Lenis()
+// const lenis = new Lenis()
 
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
+// lenis.on('scroll', (e) => {
+//   console.log(e)
+// })
 
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
+// function raf(time) {
+//   lenis.raf(time)
+//   requestAnimationFrame(raf)
+// }
 
-requestAnimationFrame(raf)
+// requestAnimationFrame(raf)
 
 gsap.registerPlugin(ScrollTrigger);
 
